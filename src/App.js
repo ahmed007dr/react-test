bd7d4726fb5b94333d504840790b5d6ac9b19f8c
ahmed007dr <ahmed007eg@gmail.com>
2024-05-24 19:27:32 +0300

    add event

--- src/App.js
+++ src/App.js
@@ -7,13 +7,27 @@ function App() {
   const name = 'ahmed mohamed'
   const title = 'learn python basic'
 
+  const hello = () =>{
+    console.log('welcome')
+  }
+  const hello2 = (name) => {
+    console.log('hello ${name}')
+  }
+
+  const hello3 = (name) => {
+    console.log('hello ${name}')
+  }
   //render logic + frontend
   return (
     <div className="App">
       <>
+      <button onClick={hello}>CLICK</button>
+      <button onClick={hello2 (' ahmed ')}> Click Parameter </button>
+      <button onClick={() => hello3 ("ahmed 3")}> Click Parameter3 </button>
+{/* 
         <Posts name={name} title={title}/>
 
-        <CreatePost />
+        <CreatePost /> */}
       </>
     </div>
   );
