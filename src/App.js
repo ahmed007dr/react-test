6b7419cbbd53d90ebb4fe018112dcc0b6bcb2a53
ahmed007dr <ahmed007eg@gmail.com>
2024-05-24 18:55:04 +0300

    define create post componan

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import './App.css';
 import Posts from './componantes/Posts';
+import CreatePost from './componantes/CreatePost';
 
 function App() {
   return (
     <div className="App">
       <>
         <Posts />
+        <CreatePost />
       </>
       
     </div>
+
   );
 }
 
---
+++ src/componantes/CreatePost.js
@@ -0,0 +1,15 @@
+const CreatePost = () =>{
+    // logic
+
+
+
+    //return logic + front end
+    return(
+       <form>
+         <textarea cols='30' rows='10'></textarea>
+         <button type='submit'> add posts </button>
+       </form>
+    )
+}
+
+export default CreatePost
